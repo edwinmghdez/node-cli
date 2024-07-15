50da0390a9a426e5e4a9cbdfbdc2e88a7bd5afe0
Edwin Gallegos <edwin.mghdez@gmail.com>
2024-07-15 15:19:01 -0600

    feat: setup project

---
+++ dist/index.js
@@ -0,0 +1,112 @@
+#!/usr/bin/env node
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (g && (g = 0, op[0] && (_ = 0)), _) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+var Command = require('commander').Command;
+var fs = require("fs");
+var path = require("path");
+var figlet = require("figlet");
+var program = new Command();
+console.log(figlet.textSync("Edwin"));
+program
+    .version("1.0.0")
+    .description("An example CLI for managing a directory")
+    .option("-l, --ls  [value]", "List directory contents")
+    .option("-m, --mkdir <value>", "Create a directory")
+    .option("-t, --touch <value>", "Create a file")
+    .parse(process.argv);
+var options = program.opts();
+function listDirContents(filepath) {
+    return __awaiter(this, void 0, void 0, function () {
+        var files, detailedFilesPromises, detailedFiles, error_1;
+        var _this = this;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    _a.trys.push([0, 3, , 4]);
+                    return [4 /*yield*/, fs.promises.readdir(filepath)];
+                case 1:
+                    files = _a.sent();
+                    detailedFilesPromises = files.map(function (file) { return __awaiter(_this, void 0, void 0, function () {
+                        var fileDetails, size, birthtime;
+                        return __generator(this, function (_a) {
+                            switch (_a.label) {
+                                case 0: return [4 /*yield*/, fs.promises.lstat(path.resolve(filepath, file))];
+                                case 1:
+                                    fileDetails = _a.sent();
+                                    size = fileDetails.size, birthtime = fileDetails.birthtime;
+                                    return [2 /*return*/, { filename: file, "size(KB)": size, created_at: birthtime }];
+                            }
+                        });
+                    }); });
+                    return [4 /*yield*/, Promise.all(detailedFilesPromises)];
+                case 2:
+                    detailedFiles = _a.sent();
+                    console.table(detailedFiles);
+                    return [3 /*break*/, 4];
+                case 3:
+                    error_1 = _a.sent();
+                    console.error("Error occurred while reading the directory!", error_1);
+                    return [3 /*break*/, 4];
+                case 4: return [2 /*return*/];
+            }
+        });
+    });
+}
+function createDir(filepath) {
+    if (!fs.existsSync(filepath)) {
+        fs.mkdirSync(filepath);
+        console.log("The directory has been created successfully");
+    }
+}
+function createFile(filepath) {
+    fs.openSync(filepath, "w");
+    console.log("An empty file has been created");
+}
+if (options.ls) {
+    var filepath = typeof options.ls === "string" ? options.ls : __dirname;
+    listDirContents(filepath);
+}
+if (options.mkdir) {
+    createDir(path.resolve(__dirname, options.mkdir));
+}
+if (options.touch) {
+    createFile(path.resolve(__dirname, options.touch));
+}
+if (!process.argv.slice(2).length) {
+    program.outputHelp();
+}
