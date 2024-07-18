a19a4abb609c629dd232503937ca18bf859e25e4
Edwin Gallegos Hernández <edwin.mghdez@gmail.com>
2024-07-18 14:48:16 -0600

    fix: remove imports without use

--- bin/index.js
+++ bin/index.js
@@ -3,16 +3,14 @@
 import chalk from "chalk";
 import inquirer from "inquirer";
 import figlet from "figlet";
-import { runBasicExample } from "../src/commands/basic-example.js";
 import { runCreateProject } from "../src/commands/create-project.js";
 
 const examples = {
-  "Basic Example": runBasicExample,
   "Create Project": runCreateProject
 };
 
 console.log(
-  chalk.blueBright(figlet.textSync("My Node CLI", { horizontalLayout: "full" }))
+  chalk.blueBright(figlet.textSync("Node CLI", { horizontalLayout: "full" }))
 );
 
 inquirer
